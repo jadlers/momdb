470151a58266b6b5eaa139e55477923728f51f66
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-10 11:13:52 +1000

    Clean up before starting actual project

--- src/App.js
+++ src/App.js
@@ -3,51 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    input: "Hello"
-  };
-
-  updateInput = event => {
-    this.setState({
-      input: event.target.value
-    });
-  };
-
-  submit = () => console.log(this.text.value);
-
   render() {
-    const welcome = "Welcome to React";
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text={welcome} />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>{this.state.input}</h3>
-        <input
-          id="controlled"
-          type="text"
-          onChange={this.updateInput}
-          defaultValue={this.state.input}
-        />
-        <input
-          id="uncontrolled"
-          type="text"
-          ref={input => (this.text = input)}
-        />
-        <button onClick={this.submit}>Show Value</button>
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return <h1 className="App-title">{text}</h1>;
   }
 }
 
