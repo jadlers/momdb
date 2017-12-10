7174701e63badbf712cf02ea2da26e05cbcd2049
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-10 13:35:15 +1000

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    ecmascript: 6,
+    jsx: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      experimentalObjectsRestSpread: true,
+      experimentalDecorators: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  extends: 'airbnb',
+};
