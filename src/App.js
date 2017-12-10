e7c3d93b24f3ad1fa626f158b273817b6658d2f2
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-10 11:08:54 +1000

    Add controlled input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    input: "Hello"
+  };
+
+  updateInput = event => {
+    this.setState({
+      input: event.target.value
+    });
+  };
+
   submit = () => console.log(this.text.value);
 
   render() {
@@ -16,7 +26,18 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="text" ref={input => (this.text = input)} />
+        <h3>{this.state.input}</h3>
+        <input
+          id="controlled"
+          type="text"
+          onChange={this.updateInput}
+          defaultValue={this.state.input}
+        />
+        <input
+          id="uncontrolled"
+          type="text"
+          ref={input => (this.text = input)}
+        />
         <button onClick={this.submit}>Show Value</button>
       </div>
     );
