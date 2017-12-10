1af7f1c13c9ec8757d940a91db049109a8771c19
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-10 12:52:17 +1000

    Fetch movies from themoviedb.org

--- src/App.js
+++ src/App.js
@@ -20,6 +20,24 @@ const movies = [
 ];
 
 class App extends Component {
+  state = {
+    movies: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=cc360d9a8f57624dc24b45671b52decf&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
