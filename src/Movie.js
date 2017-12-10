2ddca8e478101c80aecd830fee6e98dbc3535fcb
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-10 12:14:48 +1000

    Add PropTypes for Movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    })
+  };
+
   render() {
     return (
       <div>
