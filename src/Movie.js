ed91862f5c534d090a3a4fdb5d3a66966cfbc2c2
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-12 18:27:12 +1000

    Change movie titles for posters

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const POSTER_BASE_PATH = 'http://image.tmdb.org/t/p/w154';
+
 const Movie = ({ movie }) => (
   <div>
-    <h3>{movie.title}</h3>
+    <img src={`${POSTER_BASE_PATH}${movie.poster_path}`} alt={movie.title} />
   </div>
 );
 
 Movie.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
   }).isRequired,
 };
 
