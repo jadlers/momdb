ff706799295e7576f3a56d35ff330d0e0dcec01a
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-13 16:57:05 +1000

    Make App a stateless functional component(SFC)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 /* eslint react/no-did-mount-set-state: 0 */
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import MovieList from './MovieList';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Switch>
-            <Route path="/test" component={Test} />
-            <Route component={MovieList} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const Test = () => <h1>TEST</h1>;
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/test" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
-
-const Test = () => <h1>TEST</h1>;
