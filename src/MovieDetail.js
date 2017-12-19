a6a731b939164041c326378b9374a5d30f843d71
Jacob Adlers <jacob.adlers@gmail.com>
2017-12-19 18:35:42 +0800

    Use Overdrive to animate poster image

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 import { Poster } from './Movie';
 
 const POSTER_BASE_PATH = 'http://image.tmdb.org/t/p/w154';
@@ -31,10 +32,12 @@ class MovieDetail extends Component {
     return (
       <MovieWrapper backdrop={`${BACKDROP_BASE_PATH}${movie.backdrop_path}`}>
         <MovieInfo>
-          <Poster
-            src={`${POSTER_BASE_PATH}${movie.poster_path}`}
-            alt={movie.title}
-          />
+          <Overdrive id={movie.id}>
+            <Poster
+              src={`${POSTER_BASE_PATH}${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </Overdrive>
           <div>
             <h1>{movie.title}</h1>
             <h3>{movie.release_date}</h3>
