68822ece10231a5c9c08712ff4dd902c1ad7d2ba
Jacob Adlers <jacob.adlers@gmail.com>
2018-01-10 21:42:42 +0100

    Add Provider to App and init a rootReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 /* eslint react/no-did-mount-set-state: 0 */
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from './rootReducer';
 import logo from './logo.svg';
 import './App.css';
 import MovieList from './MovieList';
 import MovieDetail from './MovieDetail';
 
+const store = createStore(rootReducer);
+
 const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">
-          <img src={logo} className="App-logo" alt="logo" />
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+        </header>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
